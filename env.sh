eb821bdc5cca8e627e09c6f2d12d6bfe52f0ce41
zhoujinze <zhoujinze@gmail.com>
2022-04-25 09:51:32 +0800

    add benchmark test report

--- env.sh
+++ env.sh
@@ -1,14 +1,14 @@
 export TESTER_IP="172.20.2.115"
-export ORIGIN_IP="172.20.2.138"
+export BASELINE_IP="172.20.2.138"
 export AGENT_IP="172.20.2.212"
 
 #export TESTER_IP="host-gateway"
-#export ORIGIN_IP="host-gateway"
+#export BASELINE_IP="host-gateway"
 #export AGENT_IP="host-gateway"
 
 export TEMPO_IP=${TESTER_IP}
 
-export CUSTOMERS_ORIGIN=${ORIGIN_IP}
+export CUSTOMERS_ORIGIN=${BASELINE_IP}
 
 export CUSTOMERS_AGENT=${AGENT_IP}
 export GATEWAY_AGENT=${AGENT_IP}
